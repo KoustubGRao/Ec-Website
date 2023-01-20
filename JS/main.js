a5b7700de80785feabf63d46d7896bdaa3922b9b
code_banana <koustub.dhruva@gmail.com>
2023-01-21 01:32:55 +0530

    late night changes

--- JS/main.js
+++ JS/main.js
@@ -5,7 +5,7 @@ let dropdown2 = document.getElementById("myDropdown2");
 let accordion= document.getElementsByClassName("accordion");
 let context = document.getElementsByClassName("context");
 let image = document.getElementsByClassName("dimage");
-let highlight = document.getElementsByClassName("highlight")
+let highlight = document.getElementsByClassName("highlight");
 
 console.log("reading JS");
 
@@ -28,36 +28,41 @@ document.onclick = ()=>{
     }
 };
 
+let k=0;
 for(let i=0 ; i< accordion.length;i++)
 {
     accordion[i].addEventListener('click',()=>{
-        context[i].classList.toggle("hidden");
-        highlight[i].classList.toggle("border-l-4");
-        highlight[i].classList.toggle("mt-2");
-        highlight[i].classList.toggle("mr-2");
-        highlight[i].classList.toggle("border-blue-600");
-        for(let j=0;j<3;j++)
+        if(i!=k)
         {
-            if(i==j)
+            context[i].classList.toggle("hidden");
+            highlight[i].classList.toggle("border-l-4");
+            highlight[i].classList.toggle("mt-2");
+            highlight[i].classList.toggle("mr-2");
+            highlight[i].classList.toggle("border-blue-600");
+            for(let j=0;j<3;j++)
             {
-                image[i].classList.toggle("hidden");
-                image[j].style.opacity="1";
-            }
-            else{
-                image[j].style.opacity="0";
-                if(!image[j].matches(".hidden"))
+                if(i==j)
                 {
-                    image[j].classList.toggle("hidden");
+                    image[i].classList.toggle("hidden");
+                    image[j].style.opacity="1";
                 }
-                if(!context[j].matches('.hidden')){
-                    context[j].classList.toggle("hidden");
-                    highlight[j].classList.toggle("border-l-4");
-                    highlight[j].classList.toggle("mt-2");
-                    highlight[j].classList.toggle("mr-2");
-                    highlight[j].classList.toggle("border-blue-600");
+                else{
+                    image[j].style.opacity="0";
+                    if(!image[j].matches(".hidden"))
+                    {
+                        image[j].classList.toggle("hidden");
+                    }
+                    if(!context[j].matches('.hidden')){
+                        context[j].classList.toggle("hidden");
+                        highlight[j].classList.toggle("border-l-4");
+                        highlight[j].classList.toggle("mt-2");
+                        highlight[j].classList.toggle("mr-2");
+                        highlight[j].classList.toggle("border-blue-600");
+                    }
                 }
             }
         }
+        k=i;
     });
 
 }
